8c769e27d7325a6aa0d43d967752886e0e3c3e53
yukamikuzzzang@gmail.com <yukamikuzzzang@gmail.com>
2024-06-12 10:48:43 +0900

    웹을 만들며 가장 많이 사용하게 될 제이쿼리. 제이쿼리 사용 전 항상 먼저 링크하는 습관을 가지자.

---
+++ one/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    var c = Shape.Circle(200,200,50);
+    c.fillColor = 'green';
+
+    paper.view.draw();
+
+
+    console.log('main.js loaded');
+})
