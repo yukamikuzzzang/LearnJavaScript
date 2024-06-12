42999e12cc309256ca6de1c41cfaceedb2e3193d
yukamikuzzzang@gmail.com <yukamikuzzzang@gmail.com>
2024-06-12 11:11:46 +0900

    hello world

--- one/main.js
+++ one/main.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
 
-    var tool = new Tool();
-
-    tool.onMouseDown = function (event) {
-        //var c = Shape.Circle(event.x, event.point.y, 20);
-        var c = Shape.Circle(event.point, 20);
-        c.fillColor = 'orange';
-    };
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world'
 
     paper.view.draw();
 
