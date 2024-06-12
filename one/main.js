abd8c44e5958f951d64ac5ba5430473536332e63
yukamikuzzzang@gmail.com <yukamikuzzzang@gmail.com>
2024-06-12 11:07:47 +0900

    사용자 입력 처리하기, 사용자의 비동기적인 입력(이벤트)를 처리하는 이벤트 핸들러. 넘겨받은 매개변수를 바탕으로 추론하는 능력을 지닌 js.

--- one/main.js
+++ one/main.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
 
-    var c;
-    for (var x = 25; x < 400; x += 50) {
-        for (var y = 25; y < 400; y += 50) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = 'green';
-        }
-    }
+    var tool = new Tool();
+
+    tool.onMouseDown = function (event) {
+        //var c = Shape.Circle(event.x, event.point.y, 20);
+        var c = Shape.Circle(event.point, 20);
+        c.fillColor = 'orange';
+    };
 
     paper.view.draw();
 
